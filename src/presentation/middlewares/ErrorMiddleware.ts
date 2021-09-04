153682c6e7887e6de9e0a737a773a77684aab48a
Rogerio Sobrinho <rogerioa.sobrinho@gmail.com>
2021-09-04 14:18:09 +0100

    feat: add log in development mode

--- src/presentation/middlewares/ErrorMiddleware.ts
+++ src/presentation/middlewares/ErrorMiddleware.ts
@@ -6,10 +6,12 @@ import {
 import { AccessDeniedError } from '../../core/domain/common/exceptions/AccessDeniedError';
 import { SystemError } from '../../core/domain/common/exceptions/SystemError';
 import { InternalServerError } from '../../core/domain/common/exceptions/InternalServerError';
+import { IS_DEVELOPMENT } from '../../config/Configuration';
 
 @Middleware({ type: 'after' })
 export class ErrorMiddleware implements ExpressErrorMiddlewareInterface {
     error(err: SystemError, _: Request, res: Response) {
+        if (IS_DEVELOPMENT) console.error(err, '##');
         // Handle internal server error.
         if (!err.code || !err.httpCode) err = new InternalServerError();
         else if (err.httpCode === 403) err = new AccessDeniedError();
