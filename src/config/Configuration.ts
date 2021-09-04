d5fc32a50bb3700b0cbed331716fe4d5ab171e73
Rogerio Sobrinho <rogerioa.sobrinho@gmail.com>
2021-09-04 14:18:35 +0100

    feat: add new environments

--- src/config/Configuration.ts
+++ src/config/Configuration.ts
@@ -4,11 +4,21 @@ dotenv.config();
 // SYSTEM ENVIRONMENT
 
 export const IS_DEVELOPMENT: boolean = process.env.NODE_ENV === 'development';
+export const PROTOTYPE: string = process.env.PROTOTYPE ?? '';
+export const DOMAIN: string = process.env.DOMAIN ?? '';
+export const PROJECT_ID: string = process.env.PROJECT_ID ?? '';
+export const PROJECT_NAME: string = process.env.PROJECT_NAME ?? '';
 
 // API SERVICE
 
 export const API_PORT: number = +process.env.API_PORT ?? 8080;
 export const HOSTNAME: string = process.env.HOSTNAME ?? 'localhost';
+
+// AUTHENTICATION SERVICE
+
+export const JWT_SIGNATURE: string = process.env.JWT_SIGNATURE ?? '';
+export const JWT_SECRET_KEY: string = process.env.JWT_SECRET_KEY ?? '';
+export const JWT_EXPIRES_IN: string = process.env.JWT_EXPIRES_IN ?? '';
 
 // DATABASE CONFIGURATION
 
@@ -20,5 +30,3 @@ export const DB_PORT: number = process.env.DB_PORT
 export const DB_NAME: string = process.env.DB_NAME ?? '';
 export const DB_USER: string = process.env.DB_USER ?? '';
 export const DB_PASS: string = process.env.DB_PASS ?? '';
-
-export const PROJECT_NAME: string = process.env.PROJECT_NAME ?? 'BOOKS';
